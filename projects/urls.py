250d23777430d4e9ba1d57ffcf6ed42704539591
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-11 12:20:40 +0300

    style registration form

--- projects/urls.py
+++ projects/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('sign-up', views.sign_up, name='sign-up'),
-    # path('profile/', views.profile, name='profile'),
+    path('register/', views.register, name='register'),
+   
 ]
