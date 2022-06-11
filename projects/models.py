8f9bbecbea3e0d9731eddad862974bf7a0b69ed1
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-11 14:52:10 +0300

    add forms to allow users to update their profiles

--- projects/models.py
+++ projects/models.py
@@ -6,7 +6,7 @@ from cloudinary.models import CloudinaryField
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,)
     bio = models.CharField(default="Welcome", max_length = 40)
-    picture = CloudinaryField('images', default='https://res.cloudinary.com/dcfb3gqzg/image/upload/v1654542339/bclz5p1q2ch81qz9ukpz.jpg')
+    picture = CloudinaryField('images', default='http://res.cloudinary.com/dim8pysls/image/upload/v1639001486/x3mgnqmbi73lten4ewzv.png')
     
     def __str__(self):
         return f'{self.user.username} Profile'
