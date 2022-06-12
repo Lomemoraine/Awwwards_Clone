5ec8bbb83a340bcdf72f56f65d8e7f48580352b9
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-12 12:19:39 +0300

    write the fisrt test for project class

--- projects/tests.py
+++ projects/tests.py
@@ -15,3 +15,12 @@ class ProfileTest(TestCase):
 
     def test_delete_user(self):
         self.user.delete()
+        
+class ProjectsTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create( username='raine',password='gift1234')
+        self.project = Projects.objects.create(id=1, project_title='test post', project_image='https://ucarecdn.com/0ccf61ff-508e-46c6-b713-db51daa6626e', project_description='desc',
+                                        Author=self.user, link='http://ur.coml')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Projects))
