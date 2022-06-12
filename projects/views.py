f587f115bedb24214149c9df6709dcb787f1f3c8
Lomemoraine <lorrainechepkemoi4@gmail.com>
2022-06-12 11:37:11 +0300

    add a rating form on projects

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -107,12 +108,94 @@ def get_project(request, id):
 
     try:
         project = Projects.objects.get(pk = id)
+        ratings = Rating.objects.filter(user=request.user, project=id).first()
+        rating_status = None
+        form = RatingsForm()
+        if ratings is None:
+            rating_status = False
+        else:
+            rating_status = True
+        if request.method == 'POST':
+            form = RatingsForm(request.POST)
+            if form.is_valid():
+                rate = form.save(commit=False)
+                rate.user = request.user
+                rate.project = project
+                rate.save()
+                project_ratings = Rating.objects.filter(project=project)
+                
+                design_ratings = [d.design for d in project_ratings]
+                design_average = sum(design_ratings) / len(design_ratings)
+                
+                usability_ratings = [us.usability for us in project_ratings]
+                usability_average = sum(usability_ratings) / len(usability_ratings)
+                
+                content_ratings = [content.content for content in project_ratings]
+                content_average = sum(content_ratings) / len(content_ratings)
+                
+                score = (design_average + usability_average + content_average) / 3
+                print(score)
+                rate.design_average = round(design_average, 2)
+                rate.usability_average = round(usability_average, 2)
+                rate.content_average = round(content_average, 2)
+                rate.score = round(score, 2)
+                rate.save()
+                return HttpResponseRedirect(request.path_info)
+            else:
+                form = RatingsForm()
+                
+                
+                
+        
         
     except ObjectDoesNotExist:
         raise Http404()
     
     
-    return render(request, "project.html", {"project":project})
+    return render(request, "project.html", {"project":project,"rating_form": form,"rating_status": rating_status})
+
+#  post = Post.objects.get(title=post)
+#     ratings = Rating.objects.filter(user=request.user, post=post).first()
+#     rating_status = None
+#     if ratings is None:
+#         rating_status = False
+#     else:
+#         rating_status = True
+#     if request.method == 'POST':
+#         form = RatingsForm(request.POST)
+#         if form.is_valid():
+#             rate = form.save(commit=False)
+#             rate.user = request.user
+#             rate.post = post
+#             rate.save()
+#             post_ratings = Rating.objects.filter(post=post)
+
+#             design_ratings = [d.design for d in post_ratings]
+#             design_average = sum(design_ratings) / len(design_ratings)
+
+#             usability_ratings = [us.usability for us in post_ratings]
+#             usability_average = sum(usability_ratings) / len(usability_ratings)
+
+#             content_ratings = [content.content for content in post_ratings]
+#             content_average = sum(content_ratings) / len(content_ratings)
+
+#             score = (design_average + usability_average + content_average) / 3
+#             print(score)
+#             rate.design_average = round(design_average, 2)
+#             rate.usability_average = round(usability_average, 2)
+#             rate.content_average = round(content_average, 2)
+#             rate.score = round(score, 2)
+#             rate.save()
+#             return HttpResponseRedirect(request.path_info)
+#     else:
+#         form = RatingsForm()
+#     params = {
+#         'post': post,
+#         'ra'rating_status': rating_statusting_form': form,
+#         'rating_status': rating_status
+
+#     }
+#     return render(request, 'project.html', params)
 
 def search_projects(request):
     if 'keyword' in request.GET and request.GET["keyword"]:
